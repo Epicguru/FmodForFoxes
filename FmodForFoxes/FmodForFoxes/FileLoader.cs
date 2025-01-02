740b4948bc16ccbf1f728162fbe41e8ddec305b3
James Billy <james@epicguru.co.uk>
2025-01-02 16:49:58 +0100

    File FileLoader not closing file stream.

--- FmodForFoxes/FmodForFoxes/FileLoader.cs
+++ FmodForFoxes/FmodForFoxes/FileLoader.cs
@@ -4,7 +4,7 @@ namespace FmodForFoxes
 {
 	public static class FileLoader
 	{
-		public static string RootDirectory = "";
+		public static string RootDirectory { get; set; } = "";
 
 		/// <summary>
 		/// Loads file as a byte array.
@@ -16,7 +16,7 @@ namespace FmodForFoxes
 			// They will not work on some platforms.
 
 			// TitleContainer is cross-platform Monogame file loader.
-			var stream = TitleContainer.OpenStream(Path.Combine(RootDirectory, path));
+			using var stream = TitleContainer.OpenStream(Path.Combine(RootDirectory, path));
 
 			return LoadFileAsBuffer(stream);
 		}
@@ -26,23 +26,9 @@ namespace FmodForFoxes
 		/// </summary>
 		public static byte[] LoadFileAsBuffer(Stream stream)
 		{
-			// NOTE: Use this method to load audio files from memory 
-			// instead of built-in methods which load files directly.
-			// They will not work on some platforms.
-
-			// File is opened as a stream, so we need to read it to the end.
-			var buffer = new byte[16 * 1024];
-			byte[] bufferRes;
-			using (var ms = new MemoryStream())
-			{
-				int read;
-				while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
-				{
-					ms.Write(buffer, 0, read);
-				}
-				bufferRes = ms.ToArray();
-			}
-			return bufferRes;
+			using var memStream = new MemoryStream();
+			stream.CopyTo(memStream);
+			return memStream.ToArray();
 		}
 	}
 }
